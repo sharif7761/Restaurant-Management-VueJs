c244e04490212b7a2e3fcf5412ba8bcef9c8eb0f
ahmed7761 <sharif@namespaceit.com>
2022-06-29 01:19:48 +0600

    header component design

---
+++ src/components/Header.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="nav">
+        <a href="#">Home</a>
+        <a href="#">Add Restaurant</a>
+        <a href="#">Show Restaurants</a>
+        <a href="#">Logout</a>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'HeaderComp',
+    
+}
+</script>
+<style>
+    .nav{
+        background-color: rgb(168, 168, 220);
+        overflow: hidden;
+    }
+    .nav a{
+        padding: 20px;
+        text-decoration: none;
+        color: rgb(0, 0, 0);
+
+    }
+    
+</style>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,10 +1,15 @@
 <template>
+<HeaderComp />
     <h1>Home Page</h1>
 </template>
 
 <script>
+import HeaderComp from "./Header.vue"
 export default {
     name: 'HomePage',
+    components: {
+        HeaderComp
+    },
     mounted() {
         const user = localStorage.getItem("user-info");
         if(!user){
