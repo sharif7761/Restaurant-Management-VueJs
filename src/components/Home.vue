89537714c817e803062d61ca0966337aba882e00
ahmed7761 <sharif@namespaceit.com>
2022-06-29 23:25:13 +0600

    set user name and links to top navbar

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,17 +1,24 @@
 <template>
 <HeaderComp />
-    <h1>Home Page</h1>
+    <h1>Hello {{ name }}!! Welcome to Home Page</h1>
 </template>
 
 <script>
 import HeaderComp from "./Header.vue"
 export default {
     name: 'HomePage',
+    data(){
+        return{
+            name: ''
+        }
+    },
     components: {
         HeaderComp
     },
     mounted() {
         const user = localStorage.getItem("user-info");
+        console.log(JSON.parse(user)[0].name);
+        this.name = JSON.parse(user)[0].name;
         if(!user){
             this.$router.push({name: "SignUp"})
         }
